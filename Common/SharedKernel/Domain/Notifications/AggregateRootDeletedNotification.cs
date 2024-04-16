464a9bc65b045740efa31c7e80f8b6fbd1be1f15
SystematicChaos <yuruichen012@gmail.com>
2024-04-17 02:12:09 +0800

    添加聚合根创建、删除和修改的通知类型
    在`AggregateRootCreatedNotification.cs`、`AggregateRootDeletedNotification.cs`和`AggregateRootModifiedNotification.cs`文件中，分别添加了名为`AggregateRootCreatedNotification`、`AggregateRootDeletedNotification`和`AggregateRootModifiedNotification`的记录类型。这些都是通知类型，用于在创建、删除和更新聚合根时发送通知。这些通知包含了聚合根和其ID，可以用于获取相应的操作信息以方便管理。这些通知并非领域事件。

---
+++ Common/SharedKernel/Domain/Notifications/AggregateRootDeletedNotification.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SharedKernel.Domain.Notifications
+{
+    /// <summary>
+    /// 聚合根删除通知 (注: 用于领域内部通知，便于获取删除信息以方便管理，此通知非领域事件)
+    /// </summary>
+    public record AggregateRootDeletedNotification<T, TKey>(T AggregateRoot, TKey Id) where T : AggregateRoot<TKey>, INotification;
+}
---
+++ Common/SharedKernel/Domain/Notifications/AggregateRootModifiedNotification.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SharedKernel.Domain.Notifications
+{
+    /// <summary>
+    /// 聚合根更新通知 (注: 用于领域内部通知，便于获取更新信息以方便管理，此通知非领域事件)
+    /// </summary>
+    public record AggregateRootModifiedNotification<T, TKey>(T AggregateRoot, TKey Id) where T : AggregateRoot<TKey>, INotification;
+}
