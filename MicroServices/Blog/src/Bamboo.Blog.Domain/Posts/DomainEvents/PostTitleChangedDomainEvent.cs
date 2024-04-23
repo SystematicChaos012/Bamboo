78e6f1e29291805e34807a02d7ee698b9fca35b4
SystematicChaos012 <yuruichen012@gmail.com>
2024-04-23 10:01:38 +0800

    feat Post PostTitleChangedDomainEvent

---
+++ MicroServices/Blog/src/Bamboo.Blog.Domain/Posts/DomainEvents/PostTitleChangedDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using Bamboo.Posts.ValueObjects;
+using SharedKernel.Domain;
+
+namespace Bamboo.Posts.DomainEvents
+{
+    /// <summary>
+    /// 主题标题已更改领域事件
+    /// </summary>
+    public record PostTitleChangedDomainEvent(PostId Id, string Content) : DomainEvent
+    {
+    }
+}
--- MicroServices/Blog/src/Bamboo.Blog.Domain/Posts/Post.cs
+++ MicroServices/Blog/src/Bamboo.Blog.Domain/Posts/Post.cs
@@ -8,7 +8,7 @@ namespace Bamboo.Posts
     /// <summary>
     /// 文章
     /// </summary>
-    public sealed partial class Post 
+    public sealed partial class Post
         : AggregateRoot<PostId>
         , IConcurrencyStamp, ICreationTime, ICreator<Guid>, IModificationTime, IModifier<Guid>, IDeletionTime, IDeleter<Guid>, ILogicalDeletion
     {
@@ -40,8 +40,13 @@ namespace Bamboo.Posts
         /// <summary>
         /// 创建 Post
         /// </summary>
-        public Post(PostId id, string title, string content, Guid authorId, DateTime publicationTime) => 
+        public Post(PostId id, string title, string content, Guid authorId, DateTime publicationTime) =>
             RaiseEvent(new PostCreatedDomainEvent(id, title, content, authorId, publicationTime));
+
+        /// <summary>
+        /// 更新标题
+        /// </summary>
+        public void UpdateTitle() => RaiseEvent(new PostTitleChangedDomainEvent(Id, Content));
 
         /// <summary>
         /// 删除文章
@@ -49,10 +54,16 @@ namespace Bamboo.Posts
         public void Remove() => RaiseEvent(new PostDeletedDomainEvent(Id));
     }
 
-    partial class Post : IDomainEventApplier<PostCreatedDomainEvent>, IDomainEventApplier<PostDeletedDomainEvent>
+    partial class Post 
+        : IDomainEventApplier<PostCreatedDomainEvent>
+        , IDomainEventApplier<PostDeletedDomainEvent>
+        , IDomainEventApplier<PostTitleChangedDomainEvent>
     {
         void IDomainEventApplier<PostCreatedDomainEvent>.Apply(PostCreatedDomainEvent domainEvent) =>
             (Id, Title, Content, AuthorId, PublicationTime) = domainEvent;
+
+        void IDomainEventApplier<PostTitleChangedDomainEvent>.Apply(PostTitleChangedDomainEvent domainEvent) =>
+            (_, Title) = domainEvent;
 
         void IDomainEventApplier<PostDeletedDomainEvent>.Apply(PostDeletedDomainEvent domainEvent) { }
     }
