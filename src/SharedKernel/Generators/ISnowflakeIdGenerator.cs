ac027f06f32989fcb0724dda3bf4dd8dbb9ce6a0
SystematicChaos012 <clooooooooop@gmail.com>
2024-05-20 21:38:16 +0800

    feat ISnowflakeIdGenerator

---
+++ src/SharedKernel/Generators/ISnowflakeIdGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedKernel.Generators
+{
+    /// <summary>
+    /// 雪花 Id 生成器
+    /// </summary>
+    public interface ISnowflakeIdGenerator
+    {
+        /// <summary>
+        /// 生成雪花 Id
+        /// </summary>
+        Guid Generate();
+    }
+}
